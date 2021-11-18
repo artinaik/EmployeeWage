54c075665b1ecc1f4095d827951b573e7399a70f
Your UserName <aratinaik151997@gmail.com>
2021-11-18 17:47:05 +0530

    Feature added for calculating wages for month

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -10,6 +10,7 @@ namespace EmployeeWage
     {
         public const int isFullTime = 1;
         public const int isPartTime = 2;
+        public const int workingDays = 20;
         static void Main(string[] args)
         {
             Random random = new Random();
@@ -17,21 +18,27 @@ namespace EmployeeWage
             int empWage = 0;
             int ratePerHr = 20;
             int empHrs = 0;
-            int empcheck = random.Next(0, 3);
-            switch(empcheck)
+            int totalWage = 0;
+            for (int day = 1; day <= workingDays; day++)
             {
-                case isFullTime:
-                    empHrs = 8;
-                    break;
-                case isPartTime:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * ratePerHr;
+                totalWage = totalWage + empWage;
+                Console.WriteLine("Employee Wage is " + empWage);
             }
-            empWage = empHrs * ratePerHr;
-            Console.WriteLine("Employee Wage is " + empWage);
+            Console.WriteLine("Total Employee wage is " + totalWage);
             Console.ReadKey();
         }
     }
