088be296ad06ef5082f5e77064fe6497e1885414
Your UserName <aratinaik151997@gmail.com>
2021-11-24 14:26:50 +0530

    Added class method to compute employee wage

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -14,36 +14,42 @@ namespace EmployeeWage
         public const int maxWorkingHr = 100;
         static void Main(string[] args)
         {
-            Random random = new Random();
-           
-            int empWage = 0;
-            int ratePerHr = 20;
-            int empHrs = 0;
-            int totalWage = 0;
-            int totalEmpHr = 0;
-            int totalWorkingdays = 0;
-            while(totalEmpHr<maxWorkingHr && totalWorkingdays<workingDays)
-            {
-                totalWorkingdays++;
-                int empcheck = random.Next(0, 3);
-                switch (empcheck)
-                {
-                    case isFullTime:
-                        empHrs = 8;
-                        break;
-                    case isPartTime:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                empWage = empHrs * ratePerHr;
-                totalWage = totalWage + empWage;
-                Console.WriteLine(" Days  " + totalWorkingdays + "  Emp hrs is  " + empHrs);
-            }
-            Console.WriteLine("Total Employee wage is " + totalWage);
+            CalculateEmpWage();
             Console.ReadKey();
         }
+        public static int CalculateEmpWage()
+        {
+                Random random = new Random();
+                int empWage = 0;
+                int ratePerHr = 20;
+                int empHrs = 0;
+                int totalWage = 0;
+                int totalEmpHr = 0;
+                int totalWorkingdays = 0;
+                while (totalEmpHr < maxWorkingHr && totalWorkingdays < workingDays)
+                {
+                    totalWorkingdays++;
+                    int empcheck = random.Next(0, 3);
+                    switch (empcheck)
+                    {
+                        case isFullTime:
+                            empHrs = 8;
+                            break;
+                        case isPartTime:
+                            empHrs = 4;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+                    empWage = empHrs * ratePerHr;
+                    totalWage = totalWage + empWage;
+                Console.WriteLine(" Days  " + totalWorkingdays + "  Emp hrs is  " + empHrs);
+               
+                }
+            Console.WriteLine("Total Employee wage is " + totalWage);
+            return totalWage;
+        }
+          
     }
 }
