16398eb79b338c41f27a0e83aa05d26df21a5fa1
Your UserName <aratinaik151997@gmail.com>
2021-11-29 17:34:53 +0530

    Added feature to calculate total emp wage and save by each comapny

---
+++ EmployeeWage/EmployeeWage/EmpWageBuilderObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+   public class EmpWageBuilderObject
+    {
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+        public int totalWage = 0;
+        public int ratePerHr = 0;
+        public int maxWorkingHr=0;
+        public int maxWorkingDays = 0;
+        public string company;
+        public EmpWageBuilderObject(string company, int ratePerHr, int workingDays, int maxWorkingHr)
+        {
+            this.company = company;
+            this.ratePerHr = ratePerHr;
+            this.maxWorkingDays = workingDays;
+            this.maxWorkingHr = maxWorkingHr;
+        }
+        public void CalculateEmpWage()
+        {
+            int empHrs = 0;
+            int totalEmpHr = 0;
+            int totalWorkingdays = 0;
+            Random random = new Random();           
+            while (totalEmpHr < this.maxWorkingHr && totalWorkingdays < this.maxWorkingDays)
+            {
+                totalWorkingdays++;
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHr = totalEmpHr + empHrs;
+                //Console.WriteLine(" Days  " + totalWorkingdays + "  Emp hrs is  " + empHrs);
+            }
+            totalWage = totalEmpHr * ratePerHr;
+        }
+        public string toString()
+        {
+            return "Total Emp wage for company " + this.company + " is " + this.totalWage;
+        }
+
+    }
+}
--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -8,46 +8,16 @@ namespace EmployeeWage
 {
     class Program
     {
-        public const int isFullTime = 1;
-        public const int isPartTime = 2;
-        //public const int workingDays = 20;
-        //public const int maxWorkingHr = 100;
         static void Main(string[] args)
         {
-            CalculateEmpWage("Bridgelabz",20,20,100);
-            CalculateEmpWage("TCS", 15, 25, 100);
+            EmpWageBuilderObject bridgelabz = new EmpWageBuilderObject("bridgelabz",20,20,100);
+            bridgelabz.CalculateEmpWage();
+            Console.WriteLine(bridgelabz.toString());
+            EmpWageBuilderObject tcs = new EmpWageBuilderObject("TCS", 10, 15, 100);
+            tcs.CalculateEmpWage();
+            Console.WriteLine(tcs.toString());
             Console.ReadKey();
         }
-        public static int CalculateEmpWage(string company,int ratePerHr,int workingDays,int maxWorkingHr)
-        {
-                Random random = new Random();
-                int empHrs = 0;
-                int totalWage = 0;
-                int totalEmpHr = 0;
-                int totalWorkingdays = 0;
-                while (totalEmpHr < maxWorkingHr && totalWorkingdays < workingDays)
-                {
-                    totalWorkingdays++;
-                    int empcheck = random.Next(0, 3);
-                    switch (empcheck)
-                    {
-                        case isFullTime:
-                            empHrs = 8;
-                            break;
-                        case isPartTime:
-                            empHrs = 4;
-                            break;
-                        default:
-                            empHrs = 0;
-                            break;
-                    }
-                totalEmpHr = totalEmpHr + empHrs;
-                 Console.WriteLine(" Days  " + totalWorkingdays + "  Emp hrs is  " + empHrs);
-            }
-            totalWage = totalEmpHr*ratePerHr;
-            Console.WriteLine("Total employee wage for {0} is {1} ", company, totalWage);
-            return totalWage;
-        }
-          
+                 
     }
 }
